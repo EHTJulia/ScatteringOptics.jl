488117b15dd4831f4c695ef65f9483b854fba6ee
Documenter.jl <documenter@juliadocs.github.io>
2023-07-11 17:08:47 +0000

    build based on 0ff685b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScatteringOptics","category":"page"},{"location":"#ScatteringOptics","page":"Home","title":"ScatteringOptics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScatteringOptics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScatteringOptics]","category":"page"}]
+}
