6caefb6adb10abfb2a85af26e6b114fed1c9aff6
Anna Tartaglia <art5584@psu.edu>
2023-07-12 13:29:59 -0400

    math corrections in scattering kernel

--- src/scatteringkernels/abstractscatteringkernel.jl
+++ src/scatteringkernels/abstractscatteringkernel.jl
@@ -27,7 +27,7 @@ abstract type AbstractScatteringKernel end
 Maps D_maj(r) for given r
 Equation 33 of Psaltis et al. 2018
 """
-@inline function calc_dmaj(ps::AbstractScatteringKernel, r::Number)
+@inline function calc_dmaj(ps::AbstractScatteringKernel, λ::Number, r::Number)
     Bmaj = ps.Bmaj
     C = ps.C
     α = ps.α
@@ -36,7 +36,7 @@ Equation 33 of Psaltis et al. 2018
 
     d1 = C*(1. +ζ)/2. * Bmaj*(2. /(α*Bmaj))^(-α/(2. -α))
     d2 = (2. /(α*Bmaj))^(2. /(2. -α))
-    return d1*((1+d2*(r/rin)^2.)^(α/2.)-1)
+    return d1*((1+d2*(r/rin)^2.)^(α/2.)-1) * (λ/ps.λ0)^2.
 end
 
 """
@@ -45,7 +45,7 @@ end
 Maps D_min(r) for given r
 Equation 34 of Psaltis et al. 2018
 """
-@inline function calc_dmin(ps::AbstractScatteringKernel, r::Number)
+@inline function calc_dmin(ps::AbstractScatteringKernel, λ::Number, r::Number)
     Bmin = ps.Bmin
     C = ps.C
     α = ps.α
@@ -54,7 +54,7 @@ Equation 34 of Psaltis et al. 2018
 
     d1 = C*(1. -ζ)/2. * Bmin*(2. /(α*Bmin))^(-α/(2. -α))
     d2 = (2. /(α*Bmin))^(2. /(2. -α))
-    return d1*((1+d2*(r/rin)^2.)^(α/2.)-1)
+    return d1*((1+d2*(r/rin)^2.)^(α/2.)-1) * (λ/ps.λ0)^2.
 end
 
 """
@@ -63,12 +63,12 @@ end
 Maps phase structure function D_ϕ(r, ϕ), first converting x, y into polar coordinates
 Equation 35 of Psaltis et al. 2018
 """
-function phase_structure_point(ps::AbstractScatteringKernel, x::Number, y::Number)
+function phase_structure_point(ps::AbstractScatteringKernel, λ::Number, x::Number, y::Number)
     ϕ0 = 0 
     r = √(x^2. + y^2.)
     ϕ = atan(y,x)
-    dmaj = calc_dmaj(ps, r)
-    dmin = calc_dmin(ps, r)
+    dmaj = calc_dmaj(ps, λ, r)
+    dmin = calc_dmin(ps, λ, r)
     add = (dmaj + dmin)/2.
     sub = (dmaj - dmin)/2.
     return add + sub * cos(2. *(ϕ-ϕ0))
@@ -81,6 +81,6 @@ end
 Maps visibility kernel for a given observing wavelength λ and fourier space coordinates u, v
 """
 function visibility_point(ps::AbstractScatteringKernel, λ::Number, u::Number, v::Number)
-    b = (u*λ, v*λ)./(1+ps.M)
-    return exp(-.5 * phase_structure_point(ps, b...))
+    b = (u, v).* (λ/(1+ps.M))
+    return exp(-.5 * phase_structure_point(ps, λ, b...))
 end
