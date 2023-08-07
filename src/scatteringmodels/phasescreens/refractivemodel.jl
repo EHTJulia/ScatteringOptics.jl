4de2a5d9d0d27181d8cd028dbd2f9e06a5439c12
Anna Tartaglia <art5584@psu.edu>
2023-08-07 16:30:27 -0400

    Scatter function in refractive phase screen

--- src/scatteringmodels/phasescreens/refractivemodel.jl
+++ src/scatteringmodels/phasescreens/refractivemodel.jl
@@ -3,6 +3,21 @@ export RefractivePhaseScreen
 export phase_screen
 export wrapped_grad
 
+"""
+    RefractivePhaseScreen(sm, Nx, Ny, dx, dy, Vx_km_per_s=0.0, Vy_km_per_s=0.0)
+
+An abstract type for generating a refractive phase screen model corresponding to an image and computing the scattered
+average image.
+
+- `sm <: AbstractScatteringModel`
+- `Nx`: image size in x axis
+- `Ny`: image size in y axis
+- `dx`: pixel size in x direction (in radians)
+- `dy`: pixel size in y direction (in radians)
+
+Vx_km_per_s and Vy_km_per_s are optional for moving phase screen. 
+"""
+
 abstract type AbstractPhaseScreen end
 
 struct RefractivePhaseScreen{S, T <: Number, N<:AbstractNoiseSignal, P <: AbstractPowerSpectrumModel} <: AbstractPhaseScreen
@@ -12,8 +27,10 @@ struct RefractivePhaseScreen{S, T <: Number, N<:AbstractNoiseSignal, P <: Abstra
     signal::N  
     Q::P
     function RefractivePhaseScreen(sm::S, Nx::Number, Ny::Number, dx::T, dy::T, Vx_km_per_s=0.0::T, Vy_km_per_s=0.0::T) where {S, T}
+        # radians to cm
         dx = dx*sm.D
         dy = dy*sm.D
+
         Q = PhaseScreenPowerLaw(sm, dx, dy, Vx_km_per_s, Vy_km_per_s)
         signal = NoiseSignal((Nx, Ny))
         return new{S, T, NoiseSignal, typeof(Q)}(sm, dx, dy, signal, Q)
@@ -22,13 +39,22 @@ end
 
 StationaryRandomFields.generate_gaussian_noise(psm::AbstractPhaseScreen; rng = Random.default_rng()) = generate_gaussian_noise(psm.signal; rng = rng)
 
-function phase_screen(psm::AbstractPhaseScreen, noise_screen=nothing)
+
+function phase_screen(psm::AbstractPhaseScreen, λ_cm, noise_screen=nothing)
+    # generate noise screen if not provided
     if isnothing(noise_screen)
         noise_screen = generate_gaussian_noise(psm)
     end
+    # generate phase screen from power law and noise screen
     cns = ContinuousNoiseSignal(psm.signal)
     screengen = PSNoiseGenerator(psm.Q, cns)
-    return generate_signal_noise(screengen)
+
+    λ_bar = λ_cm/2pi
+    nx, ny = size(psm.signal)
+    FOVx = psm.dx * nx
+    FOVy = psm.dy * ny
+
+    return generate_signal_noise(screengen) .* λ_bar ./ sqrt(FOVx) ./ sqrt(FOVy)  .* nx .* ny
 end
 
 function wrapped_grad(ϕ)
@@ -48,6 +74,38 @@ function wrapped_grad(ϕ)
 end
 
 function get_rF(psm::AbstractPhaseScreen, λ_cm)
-    D, R = (psn.sm.D, psm.sm.R) .* 3.086e21
+    D, R = (psm.sm.D, psm.sm.R) 
     return √(D*R/(D+R) * λ_cm/(2.0*π))
+end
+
+function scatter(psm::RefractivePhaseScreen, imap, λ_cm; νref::Number = c_cgs)
+
+    ϕ = phase_screen(psm, λ_cm)
+
+    gradϕ_x, gradϕ_y = wrapped_grad(ϕ) ./ (psm.dx, psm.dy)
+    
+    sm = psm.sm
+    rF = get_rF(psm, λ_cm)
+    skm = kernelmodel(sm, νref=νref)
+    # convolve to get the ensemble averaged image
+    imap_ea = convolve(imap, skm)
+    rxgrid, rygrid = (imap_ea.X, imap_ea.Y) .* sm.D
+
+    interp_Iea = extrapolate(scale(interpolate(imap_ea.data.data, BSpline(Linear())), (rxgrid, rygrid)), 0.)
+    interp_gradϕx = extrapolate(scale(interpolate(gradϕ_x, BSpline(Linear())), (rxgrid, rygrid)), Periodic())
+    interp_gradϕy = extrapolate(scale(interpolate(gradϕ_y, BSpline(Linear())), (rxgrid, rygrid)), Periodic())
+
+    imap_sc = copy(imap_ea)
+    nx, ny = size(imap_sc)
+    for ix=1:nx, iy=1:ny
+        @inbounds rx = rxgrid[ix] 
+        @inbounds ry = rygrid[iy] 
+
+        rx_sft = rx .+ rF^2. * interp_gradϕx(rx, ry) 
+        ry_sft = ry .+ rF^2. * interp_gradϕy(rx, ry) 
+
+        @inbounds imap_sc[ix, iy] = interp_Iea(rx_sft, ry_sft)
+
+    end 
+    return(imap_sc)
 end
